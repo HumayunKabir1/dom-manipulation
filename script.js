eccf790fe05ce92bc7afcd5b36dadb13df945ff5
Humayun <khumayun876@gmail.com>
2018-05-30 05:04:04 +0000

    task 2c

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 /* Task 1 */
+var heading = document.getElementById('heading')
+console.log(heading.innerText)
 
-var heading = document.getElementById('heading');
-console.log(heading.InnerText);
+
 /* Task 2 using plain JavaScript */
-//task 2a
-var textBin= document.getElementById('textBin');
-textBin.addEventListener('click', function(){
-    var task2 = document.getElementById('task2a');
-    var paragraph = document.createElement('p');
-    paragraph.innerText = "Hello World";
-    task2.appendChild(paragraph);
-});
-
-/ Task 2b
-function changeBGcolor(event){
-    ver body = document.getElementsByTagName ('body'){0}
-    if(event.target.innerText=="red")
-    body.style.backgroundColor = 'red' 
+//task 2a 
+function addText2(){
+	var task2div = document.getElementById('task2a')
+	var paragraph = document.createElement('p')
+	paragraph.innerText = "Hello World"
+	task2div.appendChild(paragraph)
 }
- if(event.target.innerText=="green")
-    body.style.backgroundColor = 'green' 
-    }
-    }
-    // Task 2c
-    function redirectToGoogle (){
-        window.location.assign('http://google.com')
-    }
-    /* Tast 4 using jQuery 
+
+	var textBtn = document.getElementById('textBtn2a')
+	textBtn.addEventListener('click',addText2)
+	
+//Task 2b
+function changeBGcolor(event){
+	var body = document.getElementsByTagName('body')[0]
+	if(event.target.innerText == "Red"){
+		body.style.backgroundColor = 'red'
+	}
+	if(event.target.innerText == "Green"){
+		body.style.backgroundColor = 'green'
+	}		
+}
+//Task 2c
+function redirectToGoogle(){
+	window.location.assign('http://google.com')
+
+}
+
+/* Task 4 using jQuery
