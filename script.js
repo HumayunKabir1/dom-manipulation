854e8e997ffdd4f7cc3f09c022cdd6f587c89ca9
Humayun <khumayun876@gmail.com>
2018-05-30 04:45:50 +0000

    Task 4

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 /* Task 1 */
 
-
+var heading = document.getElementById('heading');
+console.log(heading.InnerText);
 /* Task 2 using plain JavaScript */
+//task 2a
+var textBin= document.getElementById('textBin');
+textBin.addEventListener('click', function(){
+    var task2 = document.getElementById('task2a');
+    var paragraph = document.createElement('p');
+    paragraph.innerText = "Hello World";
+    task2.appendChild(paragraph);
+});
 
-
-
-/* Task 4 using jQuery */
+/ Task 2b
+function changeBGcolor(event){
+    ver body = document.getElementsByTagName ('body'){0}
+    if(event.target.innerText=="red")
+    body.style.backgroundColor = 'red' 
+}
+ if(event.target.innerText=="green")
+    body.style.backgroundColor = 'green' 
+    }
+    }
+    // Task 2c
+    function redirectToGoogle (){
+        window.location.assign('http://google.com')
+    }
+    /* Tast 4 using jQuery 
