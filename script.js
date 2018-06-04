70f9a6411946e2af4b9a89ad113da2432993a5de
Humayun <khumayun876@gmail.com>
2018-06-04 05:11:43 +0000

    task 4ab

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
 /* Task 1 */
-var heading = document.getElementById('heading');
-console.log(heading.innerText);
 
+var heading = document.getElementById('heading')
+console.log(heading.innerText)
 /* Task 2 using plain JavaScript */
-// Task 2a
-var textBtn = document.getElementById('textBtn2');
-textBtn.addEventListener('click', function(){
-    var task2 = document.getElementById('task2a');
-    var paragraph = document.createElement('p');
-    paragraph.innerText = "Hello World";
-    task2.appendChild(paragraph);
-});
 
-// Task 2b
-function changeBGcolor(event){
-    var body = document.getElementsByTagName('body')[0];
-    if(event.target.innerText == "Red"){
-        body.style.background='red';
+var textBtn = document.getElementById('textBtn2')
+textBtn.addEventListener('click',function(){
+    var task2 = document.getElementById('task2a')
+    var paragraph =document.createElement('p')
+    paragraph.innerText ="Hello world"
+    task2.appendChild(paragraph)
+})
+
+//Task2b
+function changeBgcolor(event){
+    var body = document.getElementsByTagName('body')[0]
+    if (event.target.innerText == "Red"){
+        body.style.backgroundColor ='red'
     }
-    if(event.target.innerText == "Green"){
-        body.style.background='green';
+    if(event.target.innerText =="Green"){
+        body.style.backgroundColor = 'green'
     }
 }
-
-//Task 2c
-
-function redirectToGoogle(event){
-    window.location.assign('http://google.com');
+//task 2c
+function redirectToGoogle(){
+    window.location.assign('https://www.google.com')
 }
 
 /* Task 4 using jQuery */
 
-//Task 4a
- ('#addText4').click(function(event){
-    var paragraph =('<p>').text('Hello World')
+
+//task4a
+
+('#addText4').click(function(event){
+    var paragraph =('<p>').text('Hello world')
     ('#task4a').append(paragraph)
+    
 })
-//Task 4b
+
+//task4b
+
 function changeBG(event){
     var color = event.target.innerText.toLowerCase();
-    ('body').css('background-color', color)
+    ('body').css('backround-color,color')
 }
 
+//task4c
 ('#sum').click(function(event){
-    var sum = Number(('#num1').val())+Number (('#num2').val())
-    ('#answer').text(sum)
+    var sum =Number(('#num1').val()) +Number(('#num2').val());
+    ('#answer').text(sum);
+    
 })
